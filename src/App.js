2c90cdd32073d1654dc576f62ea1ce02fe7fed32
Mr.x <r.roshannadar@gmail.com>
2022-10-25 22:25:53 +0530

    css update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Quiz from "./quiz";
 import Layout from "./layout";
 import Todo from "./to-do";
+import Home from "./home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
           <Route path="todo" element={<Todo />} />
           <Route path="quiz" element={<Quiz />} />
         </Route>
--- src/quiz.js
+++ src/quiz.js
@@ -38,27 +38,79 @@ function Quiz() {
           isCorrect: false,
         },
       ],
-      description: "This",
+      description:
+        "We all know intuitively that not all work hours are created equal. The first hour of the morning when your energy and willpower are high is a helluva lot more productive than the hour after lunch when all you want to do is curl up and take a nap. Eat the Frog ensures that you’re using your best hours to do your most mentally taxing work and leaves less important tasks for times when you’ve already exhausted your brain power for the day.",
     },
     {
-      questionText: "The iPhone was created by which company?",
+      questionText:
+        "What should be size of the task that you choose as your frog? ",
       answerOptions: [
-        { answerText: "Apple", isCorrect: true },
-        { answerText: "Intel", isCorrect: false },
-        { answerText: "Amazon", isCorrect: false },
-        { answerText: "Microsoft", isCorrect: false },
+        {
+          answerText: "A task you can complete in 30-60 mins",
+          isCorrect: false,
+        },
+        {
+          answerText: "A task you can complete in 1-4 hours",
+          isCorrect: true,
+        },
+        {
+          answerText: "A task you can complete in less that 30 mins",
+          isCorrect: false,
+        },
+        {
+          answerText: "A task you can complete in 4-8 hours",
+          isCorrect: false,
+        },
       ],
-      description: "is",
+      description:
+        "Your frog should take half a day’s work, tops. A clearly defined, realistic task will make it easier to get started and not procrastinate on. Checking it off your list before lunch will give you a concrete win and accompanying endorphin boost to carry you into the rest of your day.",
     },
     {
-      questionText: "How many Harry Potter books are there?",
+      questionText:
+        "Why should you 'eat the frog' first thing in the morning??",
       answerOptions: [
-        { answerText: "1", isCorrect: false },
-        { answerText: "4", isCorrect: false },
-        { answerText: "6", isCorrect: false },
-        { answerText: "7", isCorrect: true },
+        {
+          answerText: "So that you can take the rest of the day off",
+          isCorrect: false,
+        },
+        {
+          answerText: "So that you can eat another frog in the afternoon",
+          isCorrect: false,
+        },
+        {
+          answerText: "So that you have the whole day to complete the task",
+          isCorrect: false,
+        },
+        {
+          answerText: "So that your most important task gets prioritized first",
+          isCorrect: true,
+        },
       ],
-      description: "Roshan",
+      description:
+        "Whatever your frog for the day happens to be, do it first thing when you sit down to work. If at all possible, don’t schedule meetings. Don’t catch up on Twitter. Don’t check your email. Don’t even think about thinking about all the other less important things you’ll have to do later in the day. Focus all of your mental energy on your frog and only your frog",
+    },
+    {
+      questionText: "How frequently should you 'eat the frog'?",
+      answerOptions: [
+        { answerText: "Once every week", isCorrect: false },
+        { answerText: "Once every fortnight", isCorrect: false },
+        { answerText: "Daily", isCorrect: true },
+        { answerText: "Whenever you need", isCorrect: false },
+      ],
+      description:
+        "Any day that you eat your frog is a good day. Furthermore, following the method means you’ll be making progress on something meaningful on a daily basis. Studies have shown that that kind of progress is a key motivator and predictor of happiness and engagement at work, and can lead to a virtuous cycle of getting things done. We feel good when we follow through on the things we intended to do which in turn makes it easier to continue doing them. When you experience a win first thing in the morning, you’re more likely to build momentum and good vibes to carry you through the rest of your workday.",
+    },
+    {
+      questionText:
+        "What important habit does 'eat the frog' help you develop? ",
+      answerOptions: [
+        { answerText: "Time management", isCorrect: false },
+        { answerText: "Multi-tasking", isCorrect: false },
+        { answerText: "Prioritization", isCorrect: false },
+        { answerText: "Deep work", isCorrect: true },
+      ],
+      description:
+        "The most valuable work in today’s knowledge economy is almost invariably work that requires all your mental resources to be focused on one thing — think cognitively demanding tasks like coding, designing, writing, strategizing, and problem-solving. Yet the modern workplace isn’t set up to support that kind of distraction-free “deep work” (a phrase coined by computer science professor Cal Newport). We’re distracted by so many emails, meetings, chat messages and requests for input that we don’t have the time or space to focus on our highest impact tasks.Eat The Frog requires us to push back against all of those distractions — both external (others interrupting us) and internal (us interrupting ourselves) — and prioritize the actions that will actually bring us closer to our goals.",
     },
   ];
 
